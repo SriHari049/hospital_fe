f991554f5e2538679f39df40e79254cb1c186803
SriHari049 <sriharimeka20@gmail.com>
2025-11-06 12:29:39 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
 import AppContextProvider from './context/AppContext.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/ecommerce">
     <AppContextProvider>
       <App />
     </AppContextProvider>
